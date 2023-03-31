3aa8472412458227a2c0911e2421ea5947382e6a
luciacantos <luciacantos22@gmail.com>
2023-03-31 11:09:19 +0200

    super pokemon earth

--- pokemon_earth.py
+++ pokemon_earth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+from pokemon import Pokemon
 """
 This Python module contains not only the class Pokemon, but also the test of
 this Python class.
@@ -35,7 +35,11 @@ this Python class.
 
 
 
-class PokemonEarth():
+class PokemonEarth(Pokemon):
+
+    def __init__(self, id, name , weapon, health, attack, defense):
+        super().__init__(id, name, weapon, health, attack, defense)
+
     """Python class to implement a basic version of a Pokemon of the game.
 
     This Python class implements the basic version of a Pokemon of the game.
